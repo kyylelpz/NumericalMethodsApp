1c952ca23ce07a6eb7bf94100b192eb61ebcc099
kyylelpz <lopez.kyle922@gmail.com>
2025-05-27 10:42:43 +0800

    added credits on console

--- app/src/main/java/numericalmethodsapp/App.java
+++ app/src/main/java/numericalmethodsapp/App.java
@@ -4,6 +4,15 @@ import numericalmethodsapp.gui.MainWindow;
 
 public class App {
     public static void main(String[] args) {
+        credits();
         MainWindow.launch(MainWindow.class, args);
+    }
+
+    public static void credits(){
+        System.out.println("Xynapse");
+        System.out.println("BSCPE-2B");
+        System.out.println("Lopez, Kyle Lemuel E.");
+        System.out.println("Jaspe, Leobert V.");
+        System.out.println("Trinidad, Jefhanie Bianca P.");
     }
 }
